9cb715391796b86610ee57ebd093959f41a46a04
noida g <suman20082020@gmail.com>
2011-04-15 15:24:07 +0530

    clearsenses theme

---
+++ clearsenses/page-node-team.tpl.php
@@ -0,0 +1,52 @@
+<?php
+// $Id: page.tpl.php,v 1.18.2.1 2009/04/30 00:13:31 goba Exp $
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language ?>" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>">
+  <head>
+		<?php print $head ?>
+    <title><?php print $head_title."This is head title" ?></title>
+    <?php print $styles ?>
+    <?php print $scripts ?>
+    <!--[if lt IE 7]>
+      <?php print phptemplate_get_ie_styles(); ?>
+    <![endif]-->
+  <style type="text/css">object,embed{visibility:hidden !important;}</style>
+   </head>
+  <body>
+<!--clearsenses-->
+<?php $path_to_theme = base_path().path_to_theme(); ?>
+<div id="container">
+<div id="main">
+<div class="logo"><img src="<?php print($path_to_theme); ?>/images/logo.gif" alt="logo"/></div>
+<div class="punchwords"><img src="<?php print($path_to_theme); ?>/images/punchwords.jpg" alt="punchwords" width="425" height="152" border="0" /><a href="mailto:hi@clearsenses.com"><img src="<?php print($path_to_theme); ?>/images/email.jpg" alt="email" /></a></div>
+</div>
+
+<div class="content">
+
+<div class="menu">
+<a href="<?php print base_path();?>"><img src="<?php print($path_to_theme); ?>/images/home.jpg" alt="home" />
+<a href="<?php print base_path(); ?>node/team"><img src="<?php print($path_to_theme); ?>/images/team.jpg" alt="team" /></a>
+<a href="<?php print base_path(); ?>node/8"><img src="<?php print($path_to_theme); ?>/images/contact_us.jpg" alt="contact us" /></a></div>
+<?php 
+$sanjay_block = module_invoke('block', 'block', 'view', 4);
+$sandeep_block = module_invoke('block', 'block', 'view', 5);
+?>
+<div class="cholder">
+  <div class="center">
+
+<div class="holder"><img alt="team" src="<?php print($path_to_theme); ?>/images/team.gif"><br>
+
+<?php print $sanjay_block['content']; ?>
+<a href="http://www.linkedin.com/in/sanjaysuri"><img width="139" height="33" alt="linked in" src="<?php print($path_to_theme); ?>/images/sanjay_in.gif"></a><br>
+<?php print $sandeep_block['content']; ?>
+<a href="http://www.linkedin.com/in/sandeepss"><img width="149" height="33" alt="sandeep" src="<?php print($path_to_theme); ?>/images/sandeep_on.gif"></a><br>
+          </div>
+</div>
+
+</div>
+
+  <?php print $closure ?>
+</div>
+  </body>
+</html>
